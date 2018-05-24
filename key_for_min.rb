3978fbca31039c761f6649eda1399f02791b6c82
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-24 17:24:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 def key_for_min_value(name_hash)
-  new_hash = {}
-  name_hash.collect do |k,v|
-  new_hash << name_hash.sort(v)
-  end 
-new_hash
-new_hash[0][v]
+  min_value = nil
+  min_key = nil
+  name_hash.each do |k, v|
+    if min_value == nil
+       min_value = v
+       min_key = k
+    elsif min_value > v
+       min_value = v
+       min_key = k
+    end
+  end
+  min_key
 end
